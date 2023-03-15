f1928047c59fbfed68ef1c7f001a8bb1d4aba8b4
VeretkoV <89856536+VeretkoV@users.noreply.github.com>
2023-03-15 22:02:47 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+  import { ref } from 'vue';
+  const count = ref(0);
+
+  const addCount = () => {
+    count.value = count.value + 1
+  }
+
+  const subtractCount = () => {
+    count.value = count.value - 1
+  }
+</script>
+
+<template>
+  <main>
+    <div>
+      <h4>The current count is...</h4>
+      <h1>
+        {{ count }}
+      </h1>
+      <button @click="subtractCount">-</button>
+      <button @click="addCount">+</button>
+    </div>
+  </main>
+</template>
+
+<style scoped >
+  main {
+    background-color: aliceblue;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  div {
+    text-align: center;
+  }
+</style>
